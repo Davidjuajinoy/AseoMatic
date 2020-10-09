79dff58e2a8fd85ad940ebe8200ecd0a764e18c8
David <Davidhernandezjuajinoy@gmail.com>
2020-10-09 11:06:04 -0500

    Error 404

--- index.php
+++ index.php
@@ -6,8 +6,29 @@ $controller = isset($_GET['c']) ? $_GET['c'] : 'All';
 $method = isset($_GET['m']) ? $_GET['m'] : 'index';
 
 $controller=$controller.'Controller';
-require_once 'controllers/'.$controller.'.php';
+$controllerFile='controllers/'.$controller.'.php';
+
+
+if(file_exists($controllerFile))
+{
+    require_once($controllerFile);
+    // se crea la instancia del controlador ejemplo (HomeController)
+    $objController = new $controller();
+    //se verifica que el metodo exista y si existe se llama
+    if(method_exists($objController,$method))
+    {
+        $objController->$method();
+    }else{
+        // echo 'no existe metodo';
+        require_once 'views/errors/error404.php';
+    }
+ 
+}
+else{
+    // echo 'no existe controller';
+    require_once 'views/errors/error404.php';
+}
 
 // call_user_func([$controller,$method]);
-$objeto = new $controller();
-$objeto->$method();
+// $objeto = new $controller();
+// $objeto->$method();
